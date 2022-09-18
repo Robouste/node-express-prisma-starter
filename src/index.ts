0b0565a747d8d3759b735972ffcf82eee0a19408
Robouste <b>
2022-09-18 02:11:41 +0200

    - init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+	process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT, 10);
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+/**
+ * endpoints go here
+ */
+
+app.use(errorHandler);
+app.use(notFoundHandler);
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
